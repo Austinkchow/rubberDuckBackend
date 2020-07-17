6ae36e185808d3fd777198b123a7db0c36dfdabf
AustinkChow <austinkchow@gmail.com>
2020-07-17 16:36:49 -0700

    change URL

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
 app.use(express.json());
 
 const corsOption = {
-    origin: "https://rubberduckinterviewer.herokuapp.com/",
+    origin: "https://rubberduckfrontend.herokuapp.com/",
     credentials: true
 }
 
